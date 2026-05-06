d80386f61466794bcfc827848442ed37780de5b8
Aldo Josue Merlos <amerlos@lxplus968.cern.ch>
2026-05-06 18:35:39 +0200

    Modified histogram settings, added some stats

--- homework_GitHub/pyscript.py
+++ homework_GitHub/pyscript.py
@@ -1,11 +1,13 @@
 import ROOT
 
-hist = ROOT.TH1F("h_gaus", "Gaussian Histogram;Value;Entries", 50, -5, 5)
+hist = ROOT.TH1F("h_gaus", "Gaussian Histogram;Value;Entries", 100, -5, 5)
 
 random = ROOT.TRandom3(0)
 
-for i in range(10000):
-    value = random.Gaus(0, 1)
+for i in range(20000):
+    value = random.Gaus(0, 1.5)
     hist.Fill(value)
 
 print("Number of entries:", hist.GetEntries())
+print("Mean:", hist.GetMean())
+print("Standard deviation:", hist.GetStdDev())
